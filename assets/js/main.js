e3793a204ca106c3e55aaff0d96423455b2ee355
Paridian92 <paridian92@hotmail.it>
2021-05-05 16:51:40 +0200

    ticket thomas

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,13 +12,18 @@ const app = new Vue({
       'assets/img/spaccio.jpg',]
   },
 
-  method:{
+  methods:{
     nextPhoto(){
       this.counterPhoto++;
-      
-    }
-    
-    
+      if(this.counterPhoto === this.images.length) this.counterPhoto = 0; 
+    },
+    prevPhoto(){
+      this.counterPhoto--;
+      if(this.counterPhoto < 0 ) this.counterPhoto = this.images.length -1; 
+    },
+    autoplay(){
+      setTimeout(nextPhoto, 3000);
+    },
   }
 
 
